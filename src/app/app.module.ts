a84d13b7237ba1149ae3f9eff910551ac03f4cf1
FOXCONN-CZ\tkalanek <tkalanek@emea.foxconnn.com>
2018-08-12 21:17:12 +0200

    Add platform state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { PlatformState } from './../store/platform/platform.state';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NgxsModule } from '@ngxs/store';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxsModule.forRoot([PlatformState]),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
